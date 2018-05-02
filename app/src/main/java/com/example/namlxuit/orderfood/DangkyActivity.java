8c0cd4fa58638900ddceea9056d83214e2539d07
Hoang Truong Huy <hoangth12495@gmail.com>
2018-05-02 23:22:24 +0700

    add attributes in Database table

--- app/src/main/java/com/example/namlxuit/orderfood/DangkyActivity.java
+++ app/src/main/java/com/example/namlxuit/orderfood/DangkyActivity.java
@@ -3,11 +3,16 @@ package com.example.namlxuit.orderfood;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.namlxuit.orderfood.database.CreateDatabase;
+
 public class DangkyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangky);
+
+        CreateDatabase createDatabase = new CreateDatabase(this);
+        createDatabase.open();
     }
 }
--- app/src/main/java/com/example/namlxuit/orderfood/database/CreateDatabase.java
+++ app/src/main/java/com/example/namlxuit/orderfood/database/CreateDatabase.java
@@ -8,9 +8,10 @@ import java.security.PublicKey;
 
 /**
  * Created by Hoang Truong Huy on 05/02/18.
+ * Created Database.
  */
 
-public class createDatabase extends SQLiteOpenHelper {
+public class CreateDatabase extends SQLiteOpenHelper {
 
     public static String TB_NHANVIEN = "NHANVIEN";
     public static String TB_MONAN = "MONAN";
@@ -48,17 +49,47 @@ public class createDatabase extends SQLiteOpenHelper {
     public static String TB_CHITIETGOIMON_SOLUONG = "SOLUONG";
 
 
-    public createDatabase(Context context) {
+    public CreateDatabase(Context context) {
         super(context,"OrderFood", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String tbNHANVIEN = "CREATE TABLE" + TB_NHANVIEN + "(" + TB_NHANVIEN_MANV + "INTERGER PRIMARY KEY AUTOINCREMENT,"
+                + TB_NHANVIEN_TENDN + "TEXT, " + TB_NHANVIEN_MATKHAU + "TEXT, " + TB_NHANVIEN_GIOITINH + "TEXT, "
+                + TB_NHANVIEN_NGAYSINH + "TEXT )";
 
+        String tbBANAN = "CREATE TABLE" + TB_BANAN + "(" + TB_BANAN_MABAN + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + TB_BANAN_TENBAN + "TEXT, " + TB_BANAN_TINHTRANG + "TEXT )";
+
+        String tbMONAN = "CREATE TABLE " + TB_MONAN + "(" + TB_MONAN_MAMONAN + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + TB_MONAN_TENMONAN + "TEXT, " +TB_MONAN_MALOAI + "INTEGER," + TB_MONAN_GIATIEN + "TEXT )";
+
+        String tbLOAIMON = "CREATE TABLE " + TB_LOAIMONAN + "(" + TB_LOAIMONAN_MALOAI + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + TB_LOAIMONAN_TENLOAI + "TEXT)";
+
+        String tbGOIMON = "CREATE TABLE" + TB_GOIMON + "(" + TB_GOIMON_MAGOIMON + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + TB_GOIMON_MABAN + "INTEGER," + TB_GOIMON_MANV + "INTEGER," + TB_GOIMON_NGAYGOI + "TEXT, "
+                + TB_GOIMON_TINHTRANG + "TEXT )";
+
+        String tbCHITIETGOIMON = "CREATE TABLE" + TB_CHITIETGOIMON + "(" + TB_CHITIETGOIMON_MAGOIMON + "INTEGER, "
+                + TB_CHITIETGOIMON_MAMONAN + "INTEGER, " + TB_CHITIETGOIMON_SOLUONG + "INTEGER, "
+                + "PRIMARY KEY ( " + TB_CHITIETGOIMON_MAGOIMON + "," + TB_CHITIETGOIMON_MAMONAN + "))";
+
+        sqLiteDatabase.execSQL(tbNHANVIEN);
+        sqLiteDatabase.execSQL(tbBANAN);
+        sqLiteDatabase.execSQL(tbMONAN);
+        sqLiteDatabase.execSQL(tbLOAIMON);
+        sqLiteDatabase.execSQL(tbGOIMON);
+        sqLiteDatabase.execSQL(tbCHITIETGOIMON);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public SQLiteDatabase open(){
+        return this.getWritableDatabase();
     }
 }
