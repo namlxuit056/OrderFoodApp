d0baeacbf67c58473b9adb168881f2249de11bf5
Hoang Truong Huy <hoangth12495@gmail.com>
2018-05-02 17:40:52 +0700

    create Databse table

--- app/src/main/java/com/example/namlxuit/orderfood/database/createDatabase.java
+++ app/src/main/java/com/example/namlxuit/orderfood/database/createDatabase.java
@@ -4,11 +4,50 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.security.PublicKey;
+
 /**
  * Created by Hoang Truong Huy on 05/02/18.
  */
 
 public class createDatabase extends SQLiteOpenHelper {
+
+    public static String TB_NHANVIEN = "NHANVIEN";
+    public static String TB_MONAN = "MONAN";
+    public static String TB_LOAIMONAN = "LOAIMONAN";
+    public static String TB_BANAN= "BANAN";
+    public static String TB_GOIMON= "GOIMON";
+    public static String TB_CHITIETGOIMON = "CHITIETGOIMON";
+
+    public static String TB_NHANVIEN_MANV = "MANV";
+    public static String TB_NHANVIEN_TENDN = "TENDN";
+    public static String TB_NHANVIEN_MATKHAU = "MATKHAU";
+    public static String TB_NHANVIEN_GIOITINH = "GIOITINH";
+    public static String TB_NHANVIEN_NGAYSINH = "NGAYSINH";
+
+    public static String TB_MONAN_MAMONAN = "MAMONAN";
+    public static String TB_MONAN_TENMONAN = "TENMONAN";
+    public static String TB_MONAN_GIATIEN = "GIATIEN";
+    public static String TB_MONAN_MALOAI = "MALOAI";
+
+    public static String TB_LOAIMONAN_MALOAI = "MALOAI";
+    public static String TB_LOAIMONAN_TENLOAI = "TENLOAI";
+
+    public static String TB_BANAN_MABAN= "MABAN";
+    public static String TB_BANAN_TENBAN= "TENBAN";
+    public static String TB_BANAN_TINHTRANG= "TINHTRANG";
+
+    public static String TB_GOIMON_MAGOIMON = "MAGOIMON";
+    public static String TB_GOIMON_MANV = "MANV";
+    public static String TB_GOIMON_NGAYGOI = "NGAYGOI";
+    public static String TB_GOIMON_TINHTRANG = "TINHTRANG";
+    public static String TB_GOIMON_MABAN= "MABAN";
+
+    public static String TB_CHITIETGOIMON_MAGOIMON = "MAGOIMON";
+    public static String TB_CHITIETGOIMON_MAMONAN = "MAMONAN";
+    public static String TB_CHITIETGOIMON_SOLUONG = "SOLUONG";
+
+
     public createDatabase(Context context) {
         super(context,"OrderFood", null, 1);
     }
