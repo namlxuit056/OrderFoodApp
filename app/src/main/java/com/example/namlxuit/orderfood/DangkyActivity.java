c3815e58ee7e2fa66d40b3d81761b61d263ee6cd
namlxuit@gmail.com <namlxuit@gmail.com>
2018-05-08 01:45:16 +0700

    xu ly conflict

--- app/src/main/java/com/example/namlxuit/orderfood/DangkyActivity.java
+++ app/src/main/java/com/example/namlxuit/orderfood/DangkyActivity.java
@@ -10,11 +10,7 @@ public class DangkyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
         setContentView(R.layout.layout_dangky);
-=======
-        setContentView(R.layout.activity_dangky);
->>>>>>> e0eb4ed416e77de1d0e4786185e157b410910161
 
         //CreateDatabase createDatabase = new CreateDatabase(this);
         //createDatabase.open();
